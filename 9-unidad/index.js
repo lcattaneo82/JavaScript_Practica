9dde72061210f8e0db2eacd008ee2d08211d0d0d
Lucas Cattaneo <cattaneolucas81@gmail.com>
2023-07-09 17:37:30 -0300

    actualizo pruebas

--- 9-unidad/index.js
+++ 9-unidad/index.js
@@ -1,1 +1,18 @@
-console.log("estoy saliendo x terminar");
+
+const logger = require('./logger')
+
+
+function elDoble(valor) {
+    if (!isNaN(valor)) {
+        return valor * 2;
+    }
+    throw new Error("El parámetro tiene que ser numérico");
+}
+
+try {
+    let variable = elDoble("S4")    
+} catch (error) {
+    logger.error(error);
+}
+
+
---
+++ 9-unidad/logger/index.js
@@ -0,0 +1,20 @@
+const winston = require('winston');
+
+const logger = winston.createLogger({
+  level: "debug",
+  format: winston.format.json(),
+
+  transports: [
+    //
+    // - Write all logs with importance level of `error` or less to `error.log`
+    // - Write all logs with importance level of `info` or less to `combined.log`
+    //
+    new winston.transports.Console(),
+    new winston.transports.File({ filename: 'logs/error.log', level: 'error' }),
+    new winston.transports.File({ filename: 'logs/combined.log' }),
+  ],
+  
+});
+
+
+module.exports = logger;
